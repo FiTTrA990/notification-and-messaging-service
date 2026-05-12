fa217a239b69bcc07090117ac48ac7b0b034cccd
Fitt <fitravg5@gmail.com>
2026-05-12 23:05:08 +0700

    Change import paths in consumer_test.go
    Updated import paths for domain and mocks.

--- internal/queue/consumer_test.go
+++ internal/queue/consumer_test.go
@@ -10,8 +10,8 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/muhammadFittra/notification-service/internal/domain"
-	"github.com/muhammadFittra/notification-service/mocks"
+	"github.com/FiTTrA990/notification-and-messaging-service/internal/domain"
+	"github.com/FiTTrA990/notification-and-messaging-service/mocks"
 )
 
 // Worker menggunakan MessageConsumer dan NotificationService.
